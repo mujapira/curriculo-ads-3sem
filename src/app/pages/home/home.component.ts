0e6bd6320f7b248ccf97ba0925b8f50af09d1abc
mujapira <malmasmello2@gmail.com>
2024-03-03 22:04:46 -0300

    Popular HTML

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -5,5 +5,8 @@ import { Component } from '@angular/core';
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
+  institutionReferenceLink:string = 'https://www.cps.sp.gov.br/fatecs/fatec-itu-dom-amaury-castanho/';
+  githubReferenceLink:string = 'https://github.com/mujapira';
+  
 
 }
